c899aa7de212491ead3bf625efee73ed10445ff6
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2024-10-29 20:35:30 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,16 @@
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer';
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer';
 import './App.css'
+import UserProfile from './components/UserProfile'; 
 
 function App() {
   return (
     <>
+    <div>
+      <UserProfile name="Alice" age={25} bio="Frontend developer and cat lover." />
+      <UserProfile name="Bob" age={30} bio="Guitarist and photographer." />
+    </div>
       <Header />
       <MainContent />
       <Footer />
